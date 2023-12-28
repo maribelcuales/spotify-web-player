5075909e4f0f4cc8b502ca8448f21ec0b44fc87c
Maribel Cuales <maribelcuales@Dantes-MacBook-Pro.local>
2023-12-28 09:39:48 +0800

    Added router get login path.

--- server/index.js
+++ server/index.js
@@ -24,13 +24,32 @@ app.listen(port, () => {
 const generateRandomString = function (length) {
     var text = '';
     var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  
+
     for (var i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
     }
     return text;
-  };
+};
 
-  
+router.get('/auth/login', (req, res) => {
+    
+    const scope = "streaming \
+                    user-read-email \
+                    user-read-private"
+
+    const state = generateRandomString(16);
+
+    const auth_query_parameters = new URLSearchParams({
+        response_type: "code",
+        client_if: spotify_client_id,
+        scope: scope,
+        redirect_url: "http://localhost:3000/auth/callback",
+        state: state
+    })
+
+    res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString());
+});
+
+
 
 
