3d713b46252696d071e1973122d567b45692eee1
Maribel Cuales <maribelcuales@Dantes-MacBook-Pro.local>
2024-01-04 23:17:33 +0800

    Implemented auth callback endpoint of the server.

--- server/index.js
+++ server/index.js
@@ -1,55 +1,75 @@
 const express = require('express');
-const dotenv = require('dotenv'); 
+const dotenv = require('dotenv');
 
-const port = 5000; 
+const port = 5000;
 
-dotenv.config()
+dotenv.config();
 
-const spotify_client_id = process.env.SPOTIFY_CLIENT_ID
-const spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
+const spotify_client_id = process.env.SPOTIFY_CLIENT_ID;
+const spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 
-const app = express(); 
+const app = express();
 
-app.get('/auth/login', (req, res) => {
-}); 
+app.get('/auth/login', (req, res) => {});
 
 app.get('/auth/callback', (req, res) => {
-}); 
+
+  const code = req.query.code; 
+
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri:"http://localhost:3000/auth/callback",
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (Buffer.from(spotify_client_id + ':' + spotify_client_secret).toString('base64')),
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    json: true
+  }; 
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      const access_token = body.access_token;
+      res.redirect('/')
+    }
+  });
+});
 
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`)
-}); 
+  console.log(`Listening at http://localhost:${port}`);
+});
 
-// State 
 const generateRandomString = function (length) {
-    var text = '';
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-    for (var i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
 };
 
 router.get('/auth/login', (req, res) => {
-    
-    const scope = "streaming \
-                    user-read-email \
-                    user-read-private"
+  const scope =   "streaming \
+                  user-read-email \
+                  user-read-private";
 
-    const state = generateRandomString(16);
+  const state = generateRandomString(16);
 
-    const auth_query_parameters = new URLSearchParams({
-        response_type: "code",
-        client_if: spotify_client_id,
-        scope: scope,
-        redirect_url: "http://localhost:3000/auth/callback",
-        state: state
-    })
+  const auth_query_parameters = new URLSearchParams({
+    response_type: "code",
+    client_id: spotify_client_id,
+    scope: scope,
+    redirect_uri: "http://localhost:3000/auth/callback",
+    state: state,
+  });
 
-    res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString());
+  res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString()
+  );
 });
-
 
 
 
